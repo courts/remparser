d6355df5c2a56c5871570fae18921c3fc06a3892
Patrick Hof <patrickhof@web.de>
2011-04-28 19:25:51 +0200

    Bugfix: Rule bodytime now converts time values from 12h to 24h format
    - Fixes bug where the rule didn't parse properly when given a time like
      '11:00am-9:00pm'

--- test/spec_remparser.rb
+++ test/spec_remparser.rb
@@ -29,7 +29,7 @@ describe RemindParser do
   end
 
   it "should parse the time given in the body if present" do
-    line = "2011/04/08 * * 180 1200 8:00-11:00pm Test Text"
+    line = "2011/04/08 * * 180 1200 8:00-11:00am Test Text"
     res = @parser.parse(line)
 
     res.content_hash.should.equal({
@@ -42,8 +42,27 @@ describe RemindParser do
           :text  => "Test Text",
           :time  => {
             :start => "8:00",
-            :end   => "11:00",
-            :tod   => "pm"
+            :end   => "11:00"
+          }
+      }
+    })
+  end
+
+  it "should parse the time given in the body if present, converting from 12h to 24h format" do
+    line = "2011/04/08 * * 600 480 8:00am-6:00pm Test Text"
+    res = @parser.parse(line)
+
+    res.content_hash.should.equal({
+      :date    => Date.parse('2011/04/08'),
+      :special => "*",
+      :tag     => "*",
+      :dur     => 600,
+      :time    => 480,
+      :body    => {
+          :text  => "Test Text",
+          :time  => {
+            :start => "8:00",
+            :end   => "18:00"
           }
       }
     })
